b5e93074ad3fbf851133cd9175eaa38f43422376
Jose Perez <joseperez@uc-3-322.AluVedruna.local>
2025-01-13 20:22:54 +0100

    Parte del Login terminada

--- src/paginas/LoginScreen.js
+++ src/paginas/LoginScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, Image } from 'react-native';
 
 export default function LoginScreen({ navigation }) {
   const [email, setEmail] = useState('');
@@ -7,26 +7,46 @@ export default function LoginScreen({ navigation }) {
 
   return (
     <View style={styles.container}>
+      {/* Imagen del logo */}
+      <Image source={require('../../assets/ic_logo 2.png')} style={styles.logo} />
+
       <Text style={styles.title}>VEDRUNA EDUCACIÓN</Text>
+
       <TextInput
         style={styles.input}
         placeholder="Introduzca su correo o nick..."
+        placeholderTextColor="#666"
         value={email}
         onChangeText={setEmail}
       />
       <TextInput
-        style={styles.input}
+        style={[styles.input, styles.passwordInput]} // Aplica un estilo adicional al campo de contraseña
         placeholder="Introduzca su contraseña..."
+        placeholderTextColor="#666"
         secureTextEntry
         value={password}
         onChangeText={setPassword}
       />
+
+      {/* Enlace de "¿Olvidaste tu contraseña?" */}
+      <TouchableOpacity style={styles.forgotPasswordContainer}>
+        <Text style={styles.forgotPassword}>¿Olvidaste tu contraseña?</Text>
+      </TouchableOpacity>
+
       <TouchableOpacity style={styles.button} onPress={() => console.log('Log In')}>
         <Text style={styles.buttonText}>Log in</Text>
       </TouchableOpacity>
-      <TouchableOpacity onPress={() => navigation.navigate('RegisterScreen')}>
-        <Text style={styles.link}>¿No tienes cuenta? Crear cuenta</Text>
-      </TouchableOpacity>
+
+      {/* Footer con separador y enlace */}
+      <View style={styles.footer}>
+        <View style={styles.separator} />
+        <TouchableOpacity onPress={() => navigation.navigate('RegisterScreen')}>
+          <Text>
+            <Text style={styles.question}>¿No tienes cuenta? </Text>
+            <Text style={styles.link}>Crear cuenta</Text>
+          </Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -39,33 +59,67 @@ const styles = StyleSheet.create({
     backgroundColor: '#121212',
     padding: 20,
   },
+  logo: {
+    width: 150,
+    height: 150,
+    marginBottom: 10,
+  },
   title: {
-    fontSize: 24,
+    fontSize: 38,
     fontWeight: 'bold',
     color: '#fff',
-    marginBottom: 20,
+    marginBottom: 40,
+    textAlign: 'center',
   },
   input: {
-    width: '100%',
+    width: '85%',
     padding: 10,
     marginBottom: 10,
-    backgroundColor: '#fff',
+    backgroundColor: '#1E1E1E',
     borderRadius: 5,
+    color: '#fff',
+  },
+  passwordInput: {
+    marginTop: 20, // Añadí un margen superior para separar del campo de email
   },
   button: {
-    width: '100%',
-    padding: 15,
-    backgroundColor: '#84bd00',
+    width: '85%',
+    padding: 10,
+    backgroundColor: '#9FC63B',
     borderRadius: 5,
     alignItems: 'center',
-    marginBottom: 10,
+    marginTop: 20,
+    marginBottom: 20,
   },
   buttonText: {
     color: '#fff',
     fontWeight: 'bold',
   },
+  forgotPasswordContainer: {
+    width: '85%',
+    marginBottom: 10,
+    alignItems: 'flex-end',
+  },
+  forgotPassword: {
+    color: '#9FC63B',
+    fontSize: 14,
+  },
+  separator: {
+    width: '100%',
+    height: 1,
+    backgroundColor: '#333',
+    marginBottom: 10,
+  },
+  footer: {
+    position: 'absolute',
+    bottom: 20,
+    width: '100%',
+    alignItems: 'center',
+  },
   link: {
-    color: '#84bd00',
-    marginTop: 10,
+    color: '#9FC63B',
+  },
+  question: {
+    color: '#dfdfdf',
   },
 });
